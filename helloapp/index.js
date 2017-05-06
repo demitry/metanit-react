8ef159fa7ebcba096b42682e556761aa3a58d04d
DESKTOP-FCMOFM8\Dmitry <aurorabitbucket@gmail.com>
2017-05-06 14:00:16 +0300

    first react

---
+++ helloapp/index.js
@@ -0,0 +1,20 @@
+// // создание элемента 
+// const title = React.createElement(
+//     'h1',   		// элемент, который мы создаем
+//     {id: 'title'},  // свойства, которые будет иметь создаваемый элемент
+//     'Hello React'   // внутреннее содержимое
+// )
+
+// // рендеринг элемента
+// ReactDOM.render(
+//    title,  // элемент, который мы хотим создать
+//    document.getElementById('container')    // где мы этот элемент хотим создать
+// )
+
+ReactDOM.render(
+    <h1 id='title'>
+        Hello React
+    </h1>,    // элемент, который мы хотим создать
+    document.getElementById("container")    // где мы этот элемент хотим создать
+)
+
